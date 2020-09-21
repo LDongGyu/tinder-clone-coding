f0caf5654c4fb6b641123f823b9cab8fda43eeaf
LDongGyu <ehdrbchd@naver.com>
2020-09-21 21:49:35 +0900

    feat: header add

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -5,9 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Let's build the TINDER Clone App!</h1>
-      {/* Header */}
       <Header />
+
       {/* Tinder Cards */}
       {/* Buttons below tinder cards */}
 
