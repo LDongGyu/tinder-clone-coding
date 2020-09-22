529dee0e0632011aa4c79f6f5e245c6b68dc6f10
LDongGyu <ehdrbchd@naver.com>
2020-09-22 20:55:39 +0900

    build: reactrouter add

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import Header from './Header';
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <h1>I am the chatpag</h1>
+          </Route>
+          <Route path="/">
+            <h1>I am the homepage</h1>
+          </Route>
+        </Switch>
+      </Router>
       {/* Tinder Cards */}
       {/* Buttons below tinder cards */}
 
