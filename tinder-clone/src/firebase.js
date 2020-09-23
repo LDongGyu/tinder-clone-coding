d703006d14712cbf8ae974b2b3cba6dee2f29889
LDongGyu <ehdrbchd@naver.com>
2020-09-23 21:23:48 +0900

    feat: TinderCards with Firebase

--- tinder-clone/src/firebase.js
+++ tinder-clone/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDkUCNJ6C9fEUsDXb5JxZ6uw7ICKBGDLJM",
   authDomain: "tinder-clone-982b3.firebaseapp.com",
@@ -8,3 +10,8 @@ const firebaseConfig = {
   appId: "1:787258107294:web:8da5a2290a580b2d2f228f",
   measurementId: "G-PHL8VS3WM9"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const database = firebaseApp.firestore();
+
+export default database;
