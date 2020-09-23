c2615af989ae1817895422cad66b4bce5bd8174d
LDongGyu <ehdrbchd@naver.com>
2020-09-23 21:03:16 +0900

    build: firebase config add

---
+++ tinder-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDkUCNJ6C9fEUsDXb5JxZ6uw7ICKBGDLJM",
+  authDomain: "tinder-clone-982b3.firebaseapp.com",
+  databaseURL: "https://tinder-clone-982b3.firebaseio.com",
+  projectId: "tinder-clone-982b3",
+  storageBucket: "tinder-clone-982b3.appspot.com",
+  messagingSenderId: "787258107294",
+  appId: "1:787258107294:web:8da5a2290a580b2d2f228f",
+  measurementId: "G-PHL8VS3WM9"
+};
